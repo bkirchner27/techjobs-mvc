b623e708010e4e987b210a44decbe58886174c38
Billy Kirchner <bkirchner27@gmail.com>
2018-11-19 19:23:48 -0600

    Search by column value controller and view set up.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -24,4 +25,20 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String results (Model model, @RequestParam String searchType, @RequestParam String searchTerm ) {
+
+
+        model.addAttribute("columns", ListController.columnChoices);
+        if (searchType.equals("All")) {
+            model.addAttribute("jobs", JobData.findAll(searchTerm));
+
+        }
+        else {
+            model.addAttribute("jobs", JobData.findByColumnAndValue(searchType, searchTerm));
+        }
+
+        return "search";
+
+    }
 }
